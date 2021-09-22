417580628252d0210a72ac768dcc39d6119f32d7
msharma38 <91138321+msharma38@users.noreply.github.com>
2021-09-22 12:11:02 +0530

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,6 +1,11 @@
-let
-  # Import sources
-  sources = import ./nix/sources.nix;
-
-# And return that specific nixpkgs
-in sources.nixpkgs
+{ system ? builtins.currentSystem }:                                                   
+                                                                                   
+let                                                                                    
+   pkgs = import <nixpkgs> { inherit system; };                                         
+                                                                                   
+   callPackage = pkgs.lib.callPackageWith (pkgs // self);                               
+                                                                                   
+   self = {                                                                             
+      lammps = callPackage ./pkgs/lammps { };                                            
+   };                                                                                   
+in self 
